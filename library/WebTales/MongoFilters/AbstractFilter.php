0bb799fdc551e3b4610e8d619f8f199fc971226b
Julien Bourdin <jbourdin@gmail.com>
2013-04-26 17:03:14 +0200

    add json result

--- library/WebTales/MongoFilters/AbstractFilter.php
+++ library/WebTales/MongoFilters/AbstractFilter.php
@@ -9,4 +9,14 @@ abstract class AbstractFilter implements IFilter
 
     public function toArray ()
     {}
+    
+    public function toJson(){
+        $resultArray = $this->toArray();
+        if(defined('JSON_PRETTY_PRINT')){
+            $resultJson = json_encode($resultArray,JSON_PRETTY_PRINT);
+        }else{
+            $resultJson = json_encode($resultArray);
+        }
+        return $resultJson;
+    }
 }
