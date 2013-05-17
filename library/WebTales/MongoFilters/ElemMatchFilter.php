7f25ce17e4a7fc0ea8958dbeb77ae544fa53f97c
Julien Bourdin <julien.bourdin@webtales.fr>
2013-05-17 10:12:04 +0200

    add elemMatch composite Filter

---
+++ library/WebTales/MongoFilters/ElemMatchFilter.php
@@ -0,0 +1,33 @@
+<?php
+namespace WebTales\MongoFilters;
+
+use WebTales\MongoFilters\CompositeFilter;
+
+class ElemMatchFilter extends CompositeFilter
+{
+    /*
+     * (non-PHPdoc) @see \WebTales\MongoFilters\AbstractFilter::toArray()
+     */
+    public function toArray ()
+    {
+    $buildFiltersArray = array();
+        foreach ($this->_filtersArray as $filter) {
+            $filterArray = $filter->toArray();
+            if (! empty($filterArray)) {
+                $buildFiltersArray[] = $filterArray;
+            }
+        }
+        if (empty($buildFiltersArray)) {
+            return array();
+        } elseif (count($buildFiltersArray) == 1) {
+            return array_pop($buildFiltersArray);
+        } else {
+            $returnArray = array(
+                '$elemMatch' => $buildFiltersArray
+            );
+            return $returnArray;
+        }
+    }
+}
+
+?>
